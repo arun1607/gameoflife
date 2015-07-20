30df9996ba06d3356ac7bcaa2d730ee7c57f07c6
Amit Shrivastava <amit.shrivastava@fidelis-inc.com>
2015-07-20 12:30:18 +0530

    Added more test cases.

--- src/test/groovy/com/app/logic/UniverseSpec.groovy
+++ src/test/groovy/com/app/logic/UniverseSpec.groovy
@@ -1,6 +1,8 @@
 package com.app.logic
 
+import com.app.exception.InvalidCellPopulationException
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Created by amits on 19/07/15.
@@ -20,5 +22,20 @@ class UniverseSpec extends Specification {
         ["XX", "XX"]             | ["----", "-XX-", "-XX-", "----"]
         ["XXX"]                  | ["--X--", "--X--", "--X--"]
         ["-X-X", "XXX-", "-X-X"] | ["------", "-XX---", "-X--X-", "-XX---", "------"]
+    }
+
+    @Unroll
+    def "verify exception in cell generation when #input cell population in given as input"() {
+
+        when:
+        new Universe(input)
+        then:
+        thrown InvalidCellPopulationException
+        where:
+        input << getData()
+    }
+
+    def getData() {
+        [null, []]
     }
 }
